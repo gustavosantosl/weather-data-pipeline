deceefa6a3d880231cdf713064c5a3e954c9311c
gustavosantosl <gustavosantos2166@gmail.com>
2026-03-03 15:48:39 -0300

    Salvando progresso local antes do pull

--- src/extract_data.py
+++ src/extract_data.py
@@ -11,23 +11,19 @@ def extract_weather_data(url:str) -> list :
     response = requests.get(url)
     data =  response.json()
 
-    if response != 200:
+    if response.status_code != 200:
         logging.error("Erro na requisição")
         return []
 
     if not data :
-        logging.warn("Nenhum dado retornado")
+        logging.warning("Nenhum dado retornado")
         return []
 
-    output_path = 'data/weather_data.json'
-    output.dir = PATH(output_path).parent
-    output.dir.mkdir(parents=True, exist_ok= True)
+    output_path = Path('data/weather_data.json')
+    output_path.parent.mkdir(parents=True, exist_ok=True)
 
     with open(output_path, 'w') as f:
         json.dump(data, f, indent=4)
 
     logging.info(f"Arquivo salvo em {output_path}")
     return data
-
-
-extract_weather_data(url)
