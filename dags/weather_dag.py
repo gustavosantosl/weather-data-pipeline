2384c9eb8b04bf918966a4eacf91d3be7bc7d8db
gustavosantosl <gustavosantos2166@gmail.com>
2026-03-03 19:33:12 -0300

    Ajustes de alguns erros

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -6,9 +6,9 @@ import os
 
 sys.path.insert(0, '/opt/airflow/src')
 
-from extract_data import extract_weather_data
-from load_data import load_weather_data
-from transform_data import data_transformations
+from src.extract_data import extract_weather_data
+from src.load_data import load_weather_data
+from src.transform_data import data_transformations
 from dotenv import load_dotenv
 
 env_path = Path(__file__).resolve().parent.parent / 'config' / '.env'
@@ -18,7 +18,7 @@ API_KEY = os.getenv('API_KEY')
 url = f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metric&appid={API_KEY}'
 
 @dag(
-    dag_id='youtube_weather_pipeline',
+    dag_id='weather_pipeline',
     default_args={
         'owner': 'airflow',
         'depends_on_past': False,
@@ -40,12 +40,12 @@ def weather_pipeline():
     @task
     def transform():
         df = data_transformations()
-        df.to_parquet('/opt/airflow/data/temp_data.parquet', index=False)
+        df.to_parquet('/home/gustavo/tutorial_pipeline/data/transformed_weather.parquet', index=False)
         
     @task 
     def load():
         import pandas as pd
-        df = pd.read_parquet('/opt/airflow/data/temp_data.parquet')
+        df = pd.read_parquet('/home/gustavo/tutorial_pipeline/data/transformed_weather.parquet')
         load_weather_data('sp_weather', df)
         
     extract() >> transform() >> load()
